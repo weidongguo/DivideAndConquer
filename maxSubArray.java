b29eec32ae765ef9094c1aaf659cd69825d369d3
Weidong Guo <weidongguo@live.com>
2015-04-15 14:04:31 -0700

    added more comments for the find optimal subArray case study

--- maxSubArray.java
+++ maxSubArray.java
@@ -5,7 +5,7 @@ class triple_t{
 	public triple_t(int i, int j, int k){
 		low = i; high = j; sum = k;
 	}
-}
+} //enable returning three thing at once without using output params
 
 public class maxSubArray{ 
 /* ************** maxCrossingSubArray() **************** */	
@@ -18,7 +18,10 @@ public class maxSubArray{
 				maxLeftSum = sum;	
 				maxLeftIndex = i;
 			}
-		}
+		} /* find the maximal sum on the left side by keep
+		   * advancing to the left, get the new sum and
+			 * compare with the maximal sum up-to-date, then
+			 * set the new maximal sum if applicable */
 
 		for(int i = mid+1, sum = 0; i <= high; i++){// the right half n/2
 			sum += ar[i];
@@ -26,19 +29,24 @@ public class maxSubArray{
 				maxRightSum = sum;
 				maxRightIndex = i;
 			}
-		}
+		} /* similar to the case (above) that advances to the left*/
+
 		return new triple_t(maxLeftIndex, maxRightIndex, maxLeftSum+maxRightSum);
 	}	// n/2 * 2 = n --> O(n)
 
-/* ******************************************** */	
+/* *************** maxSubArray() ***************************** */	
 	public static triple_t maxSubArray(int ar[], int low, int high){
 			
-		if(low == high)
+		if(low == high) //base case when there is only 1 element;
 			return new triple_t(low, high, ar[low]);	
-		int mid = (low+high)/2;			
 		
+		int mid = (low+high)/2; // locate the ~ mid point			
+	
+		/* divide the problem into sub problems, namely subarrays */	
 		triple_t leftMax = maxSubArray(ar, low, mid); // T(n/2)
 		triple_t rightMax = maxSubArray(ar, mid+1, high);	// T(n/2)
+
+		/* check the middle region that cross left and right subarrays */
 		triple_t crossingMax = maxCrossingSubArray(ar, low, mid, high);//T(n)	
 			
 		if(leftMax.sum >= rightMax.sum && leftMax.sum >= crossingMax.sum)
@@ -49,12 +57,13 @@ public class maxSubArray{
 			return crossingMax;				 
 		// max of 4 comparsions => O(1)	
 		// just some constant time comparsion to determine
-		// hether the optimal array is from the 
+		// whether the optimal array is from the 
 		// left, the middle crossing region, or the right	
 	
 	} // T(n) = 2T(n/2) + n + O(1) = O(nlgn) by the Master theorem
+
 	
-	public static triple_t maxSubArray(int ar[]){
+	public static triple_t maxSubArray(int ar[]){ //driver
 		return	maxSubArray(ar, 0, ar.length-1); 
 	}
 }
